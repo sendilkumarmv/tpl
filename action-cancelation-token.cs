5fc97b4883345ad0e3a730ee64ea6f4e26bf984b
SENDIL KUMAR <sendilkumarmurugaiah@gmail.com>
2024-10-26 18:48:12 +0530

    Update action-cancelation-token.cs

--- action-cancelation-token.cs
+++ action-cancelation-token.cs
@@ -1,5 +1,37 @@
-public static async Task Main()
+public static class Tasks
 {
-      var tokenSource = new CancellationTokenSource();
-      var token = tokenSource.Token;
+      private CancellationTokenSource source = new CancellationTokenSource();
+      private void button2_Click(object sender, EventArgs e)
+      {
+          if(source.IsCancellationRequested)
+          {
+              source = new CancellationTokenSource();
+          }
+          var token = source.Token;
+          var task = new Task(() => DoCancelableWork("You can cancel me!!!", 1000), token);
+          task.Start();
+          button2.Enabled = false;
+          btnCancelTasks.Enabled = true;
+      }
+
+      public void DoCancelableWork(string value, int delay)
+      {
+          while (true)
+          {
+              this.Invoke(new Action(() => { textBox1.Text += $"{value} | "; }));
+              Thread.Sleep(delay);
+              if(source.IsCancellationRequested)
+              {
+                  break;
+              }
+          }
+      }
+
+      private void btnCancelTasks_Click(object sender, EventArgs e)
+      {
+          source.Cancel();
+          button2.Enabled = true;
+          btnCancelTasks.Enabled = false;
+      }
+}
 }
